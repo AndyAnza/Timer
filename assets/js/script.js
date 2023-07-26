5b67b172bdf77a079db14cfbd26a5aee51233bfe
Andrea Ramirez <andrea.anza3@gmail.com>
2023-07-26 17:04:54 -0600

    Changed some styling

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,40 +1,47 @@
+const timerDiv = document.getElementById("container");
 const start = document.getElementById("startBtn");
 let countDownElement = document.getElementById("countdown");
 let restartbtn = document.createElement("button");
-let timerValue = 20;
+let timerValue = 30;
 let timerInterval;
 
-restartbtn.innerText = "Restart";
+restartbtn.innerText = "🔄";
 restartbtn.style.display = "block";
 
 function startTimer() {
   start.style.display = "none";
   timerInterval = setInterval(updateTimer, 1000);
+  timerDiv.style.backgroundColor = "#38e54d";
+  countDownElement.style.color = "black";
 }
 
 function updateTimer() {
   countDownElement.innerText = timerValue;
   timerValue--;
 
-  if (timerValue <= 10) {
+  if (timerValue < 10) {
     countDownElement.style.color = "red";
   }
 
   if (timerValue < 0) {
     clearInterval(timerInterval);
+    timerDiv.style.backgroundColor = "red";
+    countDownElement.style.color = "white";
+    timerValue = `Time Out!`;
+    countDownElement.innerText = timerValue;
     restartbtn.style.display = "block";
-    const timerDiv = document.getElementById("container");
     timerDiv.appendChild(restartbtn);
   }
 }
 
 function restartTimer() {
-  clearInterval(timerInterval);
-  timerValue = 20;
+  timerValue = 30;
   countDownElement.innerText = timerValue;
   countDownElement.style.color = "black";
   restartbtn.style.display = "none";
   start.style.display = "block";
+  timerDiv.style.backgroundColor = "#2192FF";
+  countDownElement.style.color = "white";
 }
 
 start.addEventListener("click", startTimer);
